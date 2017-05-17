3b995eff996e81b4cd2d27dc1ff7095de9b5e7f0
jalbatross <ih8u2o@gmail.com>
2017-05-17 12:32:34 -0700

    finished time server implementation

---
+++ HelloWorldExampleNetty4/src/TimeServerHandler.java
@@ -0,0 +1,35 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) { // (1)
+        final String active = new String("channel active");
+        ctx.write(active);
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+        
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                final String complete = new String("done");
+                ctx.write(complete);
+                ctx.close();
+            }
+        }); // (4)
+    }
+    
+    
+    
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
